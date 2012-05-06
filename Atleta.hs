0115755eb23b573620f5f1cb8eb112af8d78afd0
unknown <Gaston@Gaston-PC.(none)>
2012-05-05 23:41:24 -0300

    modifique deportesA que daba la capacidad tmb, capacidadA, y el aux que faltaba

--- Atleta.hs
+++ Atleta.hs
@@ -25,11 +25,15 @@ nacionalidadA (A _ _ _ pais _ _) = pais
 ciaNumberA :: Atleta -> Int
 ciaNumberA (A _ _ _ _ ciaNumber _) = ciaNumber
 
-deportesA :: Atleta -> [(Deporte, Int)]
-deportesA (A _ _ _ _ _ deportes) = deportes
+deportesA :: Atleta -> [Deporte]
+deportesA (A _ _ _ _ _ []) = []
+deportesA (A a b c d e (x:xs)) = fst x : deportesA (A a b c d e xs)
+
 
 capacidadA :: Atleta -> Deporte -> Int
-capacidadA (A _ _ _ _ _ (dep:deps)) deporte | (fst dep) == deporte = (snd dep)
+capacidadA (A _ _ _ _ _ [x]) _ = snd x
+capacidadA (A a b c d e (x:xs)) deporte | fst x == deporte = snd x
+								  | otherwise = capacidadA (A a b c d e xs) deporte
 
 auxExisteDeporte :: [(Deporte, Int)] -> Deporte -> Bool
 auxExisteDeporte [] _ = False
@@ -41,7 +45,13 @@ auxModificaCapacidad [] _ _ = []
 auxModificaCapacidad (deporte:ldeportes) depAModificar capAModificar | (fst deporte) == depAModificar = ((fst deporte), capAModificar) : (auxModificaCapacidad ldeportes depAModificar capAModificar)
 																	 | otherwise = deporte : (auxModificaCapacidad ldeportes depAModificar capAModificar)
 
---auxAgregarDeporte :: [(Deporte, Int)] -> Deporte -> Int -> [(Deporte, Int)]
+auxAgregarDeporte :: [(Deporte, Int)] -> Deporte -> Int -> [(Deporte, Int)]
+auxAgregarDeporte [] a b = [(a,b)]
+auxAgregarDeporte (xs) dep cap = (dep,cap):xs
+
 entrenarDeporteA :: Atleta -> Deporte -> Int -> Atleta
 entrenarDeporteA (A nombre sexo anio pais cia deportes) depPorAgregar capPorAgregar | auxExisteDeporte deportes depPorAgregar = (A nombre sexo anio pais cia (auxModificaCapacidad deportes depPorAgregar capPorAgregar) )
---																					| otherwise = (A nombre sexo anio pais cia (auxAgregarDeporte deportes depPorAgregar capPorAgregar))
+																				    | otherwise = (A nombre sexo anio pais cia (auxAgregarDeporte deportes depPorAgregar capPorAgregar))
+
+atleta :: String->Sexo->Int->Pais->Int->[(Deporte, Int)]-> Atleta
+atleta a b c d e f = A a b c d e f
