2b2580dffa838b1f6647e3ed4d918008a53f0df6
Lautaro Petaccio <lausuper@gmail.com>
2012-05-07 17:01:22 -0300

    Agrego testJuegos para hacer pruebas

--- TPF.hs
+++ TPF.hs
@@ -4,3 +4,5 @@ import Tipos
 import Atleta
 import Competencia
 import JJOO
+
+testJuegos = NuevoDia [(Participar (A "reloco" Masculino 1991 "arg" 2 []) (C ("deporte", Masculino)))] (NuevoDia [(Participar (A "pepe" Masculino 1991 "arg" 1 []) (C ("deporte", Masculino)))] (J 12 [(A "pepe" Masculino 1991 "arg" 1 [])] 1))
