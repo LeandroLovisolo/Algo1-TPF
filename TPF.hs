8f070400243eacd76823acadd0cbee4bc50387a4
Lautaro Petaccio <lausuper@gmail.com>
2012-05-08 11:15:50 -0300

    Modificada testJuegos para que funcione bien

--- TPF.hs
+++ TPF.hs
@@ -5,6 +5,6 @@ import Atleta
 import Competencia
 import JJOO
 
---testJuegos = NuevoDia [(Participar (A "reloco" Masculino 1991 "arg" 2 []) (C ("deporte", Masculino)))] (NuevoDia [(Participar (A "pepe" Masculino 1991 "arg" 1 []) (C ("deporte", Masculino)))] (J 12 [(A "pepe" Masculino 1991 "arg" 1 []), (A "pepe" Masculino 1991 "arg" 2 []), (A "pepe" Masculino 1991 "arg" 3 [])] 1))
+--testJuegos = NuevoDia [(Participar (A "reloco" Masculino 1991 "arg" 2 [("football", 12)]) (C ("football", Masculino)))] (NuevoDia [(Participar (A "pepe" Masculino 1991 "arg" 1 [("tenis",50)]) (C ("tenis", Masculino)))] (J 12 [(A "pepe" Masculino 1991 "arg" 1 [("tenis", 50)]), (A "pepe" Masculino 1991 "arg" 2 [("football", 12)]), (A "pepe" Masculino 1991 "arg" 3 [])] 1))
 --testAtletas = [(A "pepe" Masculino 1991 "arg" 1 []), (A "pepa" Masculino 1991 "arg" 2 []), (A "pepo" Masculino 1991 "arg" 3 [])]
 --testAtletasx = [(A "pepe" Masculino 1991 "arg" 1 []), (A "pepa" Masculino 1991 "arg" 2 [])]
