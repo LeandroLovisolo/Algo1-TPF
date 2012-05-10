d57c8d3a3225bade7fca565832f03f64ea356619
Leandro Lovisolo <leandro@leandro.me>
2012-05-10 07:53:51 -0300

    Agregué módulo Tests y una librería para escribir tests en Haskell

---
+++ Tests.hs
@@ -0,0 +1,157 @@
+-------------------------------------------------------------------------------
+-- Para correr los tests, cargar este módulo en la consola de Hugs           --
+-- ejecutando el siguiente comando desde el directorio raíz del proyecto:    --
+--                                                                           --
+-- hugs -P{HUGS}/*:.:hunit Tests                                             --
+--                                                                           --
+-- Reemplazando {HUGS} por el directorio en donde se instalaron los paquetes --
+-- de Hugs. Por ejemplo, si dicho directorio fuera /usr/lib/hugs/packages,   --
+-- el comando a correr sería el siguiente:                                   --
+--                                                                           --
+-- hugs -P/usr/lib/hugs/packages/*:.:hunit Tests                             --
+--                                                                           --
+-- Una vez cargado el módulo en el intérprete, se pueden correr los tests    --
+-- tipeando lo siguiente:                                                    --
+--                                                                           --
+-- runtests                                                                  --
+--                                                                           --
+-- Este comando imprime un reporte en la consola con el éxito o fracaso de   --
+-- los tests.                                                                --
+--                                                                           --
+-- Los tests fueron implementados usando la librería HUnit. Más información  --
+-- en la siguiente URL:                                                      --
+--                                                                           --
+-- http://hunit.sourceforge.net/HUnit-1.0/Guide.html                         --
+-------------------------------------------------------------------------------
+
+
+module Tests where
+
+import HUnit
+
+import Tipos
+import Atleta
+import Competencia
+import JJOO
+
+
+runtests = runTestTT tests
+
+
+-------------------------------------------------------------------------------
+-- Tests ----------------------------------------------------------------------
+-------------------------------------------------------------------------------
+
+
+-- Sintaxis para agregar tests nuevos:
+-- 
+--   "nombre del test" :~ valorEsperado @=? expresion
+--
+-- Donde:
+--   (*) "nombre del test" es un string que describe *brevemente* lo que
+--       se intenta testear;
+--   (*) valorEsperado es el valor que debería devolver la expresión testeada
+--       para considerarse correcta;
+--   (*) expresion es el código que nos interesa evaluar para luego comparar
+--       el valor devuelto contra valorEsperado.
+--
+-- Ejemplo:
+-- 
+--   "la suma de 2 y 2 es 4" :~ 4 @=? (2+2)
+--
+-- En este ejemplo se intenta verificar que el resultado devuelto por (2+2)
+-- sea en efecto 4, y llamamos al test "la suma de 2 y 2 es 4".
+
+
+tests = TestList [
+        "dePaseoJ: devuelve los atletas correctos" ~:
+            [555, 666] @=? map ciaNumberA (dePaseoJ dataDePaseoJ),
+
+        "transcurrirDiaJ: jornada actual correcta" ~:
+            3 @=? jornadaActualJ (transcurrirDiaJ dataTranscurrirDiaJ),
+
+        "transcurrirDiaJ: el día transcurrido tiene la cantidad de competencias correcta" ~:
+            3 @=? length (cronogramaJ (transcurrirDiaJ dataTranscurrirDiaJ) 2),
+
+        "transcurrirDiaJ: las competencias del día transcurrido fueron finalizadas" ~:
+            True @=? all finalizadaC (cronogramaJ (transcurrirDiaJ dataTranscurrirDiaJ) 2),
+
+        "transcurrirDiaJ: las competencias de los demas días no se modificaron" ~:
+            True @=? (map finalizadaC (cronogramaJ dataTranscurrirDiaJ 1)) ==
+                     (map finalizadaC (cronogramaJ (transcurrirDiaJ dataTranscurrirDiaJ) 1)),
+
+        "transcurrirDiaJ: ranking Volley correcto" ~:
+            [555, 222, 444, 111, 666, 888, 777, 333] @=?
+                map ciaNumberA (rankingC ((cronogramaJ (transcurrirDiaJ dataTranscurrirDiaJ) 2) !! 0)),
+
+        "transcurrirDiaJ: ranking Arqueria correcto" ~:
+            [888, 777, 666, 555, 444, 333, 222, 111] @=?
+                map ciaNumberA (rankingC ((cronogramaJ (transcurrirDiaJ dataTranscurrirDiaJ) 2) !! 1)),
+
+        "transcurrirDiaJ: ranking Natacion correcto" ~:
+            [888, 777, 666, 555, 444, 333, 222, 111] @=?
+                map ciaNumberA (rankingC ((cronogramaJ (transcurrirDiaJ dataTranscurrirDiaJ) 2) !! 2)),
+
+        "medalleroJ: devuelve los valores correctos" ~:
+            [("Argentina", [2, 1, 0]),
+             ("Ecuador",   [1, 1, 0]),
+             ("Chile",     [1, 0, 2]),
+             ("Brasil",    [0, 2, 1]),
+             ("Dinamarca", [0, 0, 1])] @=? medalleroJ dataMedalleroJ
+             
+    ]
+
+
+-------------------------------------------------------------------------------
+-- Datos de prueba ------------------------------------------------------------
+-------------------------------------------------------------------------------
+
+
+dataDePaseoJ :: JJOO
+dataDePaseoJ = NuevoDia dia2 (NuevoDia dia1 (J 2012 (atletasActivos ++ atletasDePaseo) 1))
+    where atletasActivos = [(nuevoA "Abel"     Masculino 18 "Argentina" 111),
+                            (nuevoA "Beto"     Masculino 19 "Brasil"    222),
+                            (nuevoA "Carlos"   Masculino 20 "Chile"     333),
+                            (nuevoA "Daniel"   Masculino 21 "Dinamarca" 444)]
+          atletasDePaseo = [(nuevoA "Esteban"  Masculino 22 "Ecuador"   555),
+                            (nuevoA "Federico" Masculino 22 "Francia"   666)]
+          dia1 = [(competencia "Futbol"),
+                  (competencia "Handball"),
+                  (competencia "Basket")]
+          dia2 = [(competencia "Volley"),
+                  (competencia "Arqueria"),
+                  (competencia "Natacion")]
+          competencia d = (nuevaC d Masculino atletasActivos)
+
+dataMedalleroJ :: JJOO
+dataMedalleroJ = dataTranscurrirDiaJ
+
+dataTranscurrirDiaJ :: JJOO
+dataTranscurrirDiaJ = NuevoDia dia3 (NuevoDia dia2 (NuevoDia dia1 (J 2012 dataAtletas 2)))
+    where cronograma = [dia1, dia2, dia3]
+          dia1 = [(competenciaF "Futbol"   [111, 222, 333, 555, 444, 777, 888, 666]),
+                  (competenciaF "Handball" [333, 111, 222, 888, 555, 444, 777, 666]),
+                  (competenciaF "Basket"   [111, 555, 333, 444, 888, 222, 666, 777])]
+          dia2 = [(competenciaF "Volley"   [555, 222, 444, 111, 666, 888, 777, 333]),
+                  (competencia  "Arqueria"),
+                  (competencia  "Natacion")]
+          dia3 = [(competencia  "Gimnasia Artistica"),
+                  (competencia  "Hockey"),
+                  (competencia  "Rugby")]
+          competenciaF dep pos = finalizarC (nuevaC dep Masculino dataAtletas) pos []
+          competencia  dep     = (nuevaC dep Masculino dataAtletas)
+
+dataAtletas :: [Atleta]
+dataAtletas = map entrenarDeportes atletas 
+  where atletas = [(nuevoA "Abel"     Masculino 18 "Argentina" 111),
+                   (nuevoA "Beto"     Masculino 19 "Brasil"    222),
+                   (nuevoA "Carlos"   Masculino 20 "Chile"     333),
+                   (nuevoA "Daniel"   Masculino 21 "Dinamarca" 444),
+                   (nuevoA "Esteban"  Masculino 22 "Ecuador"   555),
+                   (nuevoA "Federico" Masculino 22 "Francia"   666),
+                   (nuevoA "Gabriel"  Masculino 22 "Grecia"    777),
+                   (nuevoA "Horacio"  Masculino 22 "Honduras"  888)]
+        deportes = ["Futbol", "Handball", "Basket", "Volley", "Arqueria", "Natacion"]
+        entrenarDeportes atleta = foldl entrenarDeporte atleta deportes
+        entrenarDeporte atleta deporte = entrenarDeporteA atleta deporte (capacidad atleta)
+        capacidad atleta = ciaNumberA atleta
